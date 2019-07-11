55cc6810903761e12088acdabe4df96cdae6f4ff
Andre Artelt <aartelt@techfak.uni-bielefeld.de>
2019-07-11 17:04:17 +0200

    docs: conf.py: Update autodoc_mock_imports

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'André Artelt'
 
 # -- General configuration ---------------------------------------------------
 
-autodoc_mock_imports = ["torch", "tensorflow", "sklearn", "jax", "jaxlib", "numpy", "scipy", "sklearn-lvq"]
+autodoc_mock_imports = ["torch", "tensorflow", "sklearn", "jax", "jaxlib", "numpy", "scipy", "sklearn_lvq"]
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
