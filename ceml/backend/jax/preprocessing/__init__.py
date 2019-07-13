a5be8919f9d34b6965115b86d9f5105b2d2362f6
Andre Artelt <aartelt@techfak.uni-bielefeld.de>
2019-07-13 21:16:33 +0200

    Merge min_max_scaler.py into scaler.py

--- ceml/backend/jax/preprocessing/__init__.py
+++ ceml/backend/jax/preprocessing/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from .scaler import *
 from .normalizer import *
-from .min_max_scaler import *
 from .pca import *
 from .polynomial_features import *
--- ceml/backend/jax/preprocessing/scaler.py
+++ ceml/backend/jax/preprocessing/scaler.py
@@ -17,3 +17,20 @@ class StandardScaler(Model):
         Computes the forward pass.
         """
         return (x - self.mu) / self.sigma
+
+
+class MinMaxScaler(Model):
+    """
+    Wrapper for the min max scaler.
+    """
+    def __init__(self, min_, scale):
+        self.min = min_
+        self.scale = scale
+        
+        super(MinMaxScaler, self).__init__()
+    
+    def predict(self, x):
+        """
+        Computes the forward pass.
+        """
+        return self.scale * x + self.min
